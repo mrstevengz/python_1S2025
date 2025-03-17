f5e4221e88b7ca67399e87a22a5d24208f75cf88
mrstevengz <ssequeira@uamv.edu.ni>
2025-03-17 10:31:26 -0600

    Sesion 1 17 de marzo

---
+++ example.py
@@ -0,0 +1,11 @@
+"""
+Imprimir los datos personales 
+de estudiante
+"""
+
+print("Datos del estudiante")
+print("="*20)
+print("Nombre: ", end="")
+print("Jose Maria")
+print("Apellidos:", end=" ")
+print("Lopez Gutierrez")
